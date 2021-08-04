446270379de540a8b905ad45e64adb83839b0a45
AllisonAnz <AllisonAnzalone@gmail.com>
2021-08-04 10:10:13 -0500

    Assignment Completed

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,15 @@
 # Build your say_hello method here
+# Build a method that can be called with an optional argument by defining that method with a defualt argument 
+
+# say_hello
+#   accepts an argument of a name and prints out Hello with that Name 
+#   defaults to Ruby Programmer when no name is passed in 
+
+def say_hello(name="Ruby Programmer")
+    puts "Hello #{name}!"
+end
+
+say_hello("Kent Beck")
+# => Hello Kent Beck!
+say_hello
+#=> Hello Ruby Programmer!
